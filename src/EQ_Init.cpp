4a191cae08f3db44a804c004a76cb3f9b3305997
Martijn Manneveld <martijn.manneveld@gmail.com>
2015-03-03 21:57:22 +0100

    v0.0.4
    Minor changes

--- src/EQ_Init.cpp
+++ src/EQ_Init.cpp
@@ -1,5 +1,6 @@
 #include "EQ.h"
 #include "Constants.h"
+#include<iostream>
 
 bool EQ::Init()
 {
@@ -8,8 +9,9 @@ bool EQ::Init()
         return false;
     }
 
-     if((Window = SDL_CreateWindow("EpicQuest",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
+    if((Window = SDL_CreateWindow("EpicQuest",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN)) == NULL)
     {
+        std::cout << "Window Creation Error" << SDL_GetError() << std::endl;
         return false;
     }
     return true;
